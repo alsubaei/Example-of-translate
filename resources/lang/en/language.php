2bb68a050a23c5b6caf224f2ec90936c33be7742
Manal Al-Subaei <alsubaeimanal@gmail.com>
2021-10-24 23:49:41 +0300

    Example of translate

---
+++ resources/lang/en/language.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'title'=>'Making your ideas
+happen!',
+    'CLIENTS'=>'Sed tamen tempor magna labore dolore dolor sint tempor duis magna elit veniam aliqua esse amet veniam enim export quid quid veniam aliqua eram noster malis nulla duis fugiat culpa esse aute nulla ipsum velit export irure minim illum fore'
+];
--- resources/views/pages/IntroSection.blade.php
+++ resources/views/pages/IntroSection.blade.php
@@ -1,7 +1,7 @@
 <section id="intro">
 
         <div class="intro-content">
-            <h2>Making <span>your ideas</span><br>happen!</h2>
+            <h2>{{trans('language.title')}}</h2>
             <div>
                 <a href="#about" class="btn-get-started scrollto">Get Started</a>
                 <a href="#portfolio" class="btn-projects scrollto">Our Projects</a>
